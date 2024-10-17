c2c79c71db16a462d6bf5af72831581722d0ebda
Pedro Henrique <166777715+pedrohenriquefc@users.noreply.github.com>
2024-10-16 23:34:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function geoFindMe() {
     const status = document.querySelector("#status");
     const mapLink = document.querySelector("#map-link");
+    const lista = document.querySelector("#lista");
 
     mapLink.href = "";
     mapLink.textContent = "";
+
+    function adicionaItem(value) {
+        var novoItem = document.createElement("li");
+        novoItem.textContent = value;
+        lista.appendChild(novoItem);
+    }
 
     function success(position) {
         const latitude = position.coords.latitude;
@@ -12,6 +19,7 @@ function geoFindMe() {
         status.textContent = "";
         mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
         mapLink.textContent = `Latitude: ${latitude}, Longitude ${longitude}`;
+        adicionaItem(mapLink.textContent);
     }
 
     function error() {
